52b1b53e480fabeab929dd0120f1fb113aacaf03
RamiVishva <vishvarami027@gmail.com>
2023-02-26 23:53:12 +0530

    Week 1 task

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Component/Title';
+import Home from './Pages/Home';
+import Counter from './Component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Title title="Title-1" des="Description-1"></Title>
+      <Title title="Title-2" des="Description-2"></Title>
+      <Title title="Title-3" des="Description-3"></Title>
+      <Home></Home>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         title:"Title from home",
+         des:"Description from home"
+      }
+    }
+    
+  render() {
+    return (
+      <>
+        <br></br>
+        <h1>{this.state.title}</h1> 
+        <h2>{this.state.des}</h2>
+      </>
+    )
+  }
+}
+
+export default Home
